b802f96fec0e4cfc3fd664c9bccd370478dab0a9
Gustavo Barros <77308865+arg-07@users.noreply.github.com>
2023-06-29 21:09:29 -0300

    Aula de C# em .NET - POO - Exercícios (Código com falhas)

--- ProjetoAulaClasses/Program.cs
+++ ProjetoAulaClasses/Program.cs
@@ -1,9 +1,5 @@
-﻿
-
-using ProjetoAulaClasses;
-using System;
+﻿using System;
 using System.Globalization;
-using System.Security.Cryptography.X509Certificates;
 
 namespace Course
 {
@@ -12,37 +8,78 @@ namespace Course
         static void Main(string[] args)
         {
 
-            Produto p = new Produto("tv", 500.00, 10);
+            ContaBancaria conta;
 
-            //p.SetNome("T");
-            p.Nome = "T";
-         
-            Console.WriteLine(p.Nome);
-            Console.WriteLine(p.GetQuantidade());
-            Console.WriteLine(p.Preco);
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
 
-            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
 
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
         }
-
     }
-
 }
 
 
 
 
 
+//using ProjetoAulaClasses;
+//using System;
+//using System.Globalization;
+//using System.Security.Cryptography.X509Certificates;
 
+//namespace Course
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
+//            Produto p = new Produto("tv", 500.00, 10);
 
+//            //p.SetNome("T");
+//            p.Nome = "T";
 
+//            Console.WriteLine(p.Nome);
+//            Console.WriteLine(p.GetQuantidade());
+//            Console.WriteLine(p.Preco);
 
+//            Console.ReadLine();
 
+//        }
 
+//    }
 
-
-
+//}
 
 
 
